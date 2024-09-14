ef2c852489ad0f376ef6eef6a296a8c56111507d
Cody Bumgardner <cody@uky.edu>
2024-09-14 15:39:35 -0400

    update configs

--- convert_dinov2_to_hf.py
+++ convert_dinov2_to_hf.py
@@ -540,7 +540,8 @@ def convert_dinov2_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub=
 
 
 
-    pixel_values = processor(image, return_tensors="pt", data_format=ChannelDimension.FIRST, input_data_format=ChannelDimension.FIRST).pixel_values
+    #pixel_values = processor(image, return_tensors="pt", data_format=ChannelDimension.FIRST, input_data_format=ChannelDimension.FIRST).pixel_values
+    pixel_values = processor(image, return_tensors="pt").pixel_values
 
     print(type(original_pixel_values))
     print(original_pixel_values.shape)
