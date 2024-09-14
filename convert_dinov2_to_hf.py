414974a98f9733c531f5558d7b2018822f35f50f
Cody Bumgardner <cody@uky.edu>
2024-09-14 14:57:11 -0400

    update configs

--- convert_dinov2_to_hf.py
+++ convert_dinov2_to_hf.py
@@ -415,6 +415,8 @@ def convert_dinov2_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub=
     pixel_values = processor(image, return_tensors="pt").pixel_values
 
     print(original_pixel_values.shape)
+    print(pixel_values.shape)
+
     exit(0)
 
     assert torch.allclose(original_pixel_values, pixel_values)
