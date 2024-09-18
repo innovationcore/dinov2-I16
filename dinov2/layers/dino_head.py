4d7769b9df50161ca75b4483ee80828530714765
Cody Bumgardner <cody@uky.edu>
2024-09-17 20:53:58 -0400

    updates to support native single channel 16bit

--- dinov2/layers/dino_head.py
+++ dinov2/layers/dino_head.py
@@ -38,6 +38,7 @@ class DINOHead(nn.Module):
     def forward(self, x):
         x = self.mlp(x)
         eps = 1e-6 if x.dtype == torch.float16 else 1e-12
+        logger.error("DTYPE: " + str(x.dtype))
         x = nn.functional.normalize(x, dim=-1, p=2, eps=eps)
         x = self.last_layer(x)
         return x
