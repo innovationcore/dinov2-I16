4729b9de40399dcdbbbd20b341710a2f2feee2cd
Cody Bumgardner <cody@uky.edu>
2024-09-19 17:45:28 -0400

    updates to support native single channel 16bit

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ export OMP_NUM_THREADS=1
 unset "${!SLURM@}"
 #HACK
 
-torchrun --nproc_per_node=1 dinov2/train/train.py --config-file=$1.yaml --output-dir=/workspace/$1
+torchrun --nproc_per_node=1 dinov2/train/train.py --config-file=$1.yaml --output-dir=/workspace/dinov2/$1
