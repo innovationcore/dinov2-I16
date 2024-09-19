30982e830bfce77cd55afc501c9ef3927711af1c
Cody Bumgardner <cody@uky.edu>
2024-09-19 12:43:44 -0400

    updates to support native single channel 16bit

--- dinov2/data/datasets/myunidataset.py
+++ dinov2/data/datasets/myunidataset.py
@@ -54,7 +54,7 @@ class MyUniDataset(ExtendedVisionDataset):
             image, target = self.transforms(image, target)
 
         logger.info("3 img type: " + str(type(image)))
-        logger.info("3 img : " + str(image))
+        logger.info("3 img keys: " + str(image.keys()))
         #logger.info("3 img : " + str(image))
         exit()
 
