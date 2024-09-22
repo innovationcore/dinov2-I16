36d7c84d1e5de4fbe002fcf41f747766f54f4cec
Cody Bumgardner <cody@uky.edu>
2024-09-22 19:34:45 -0400

    fixing conversion

--- dinov2/fsdp/__init__.py
+++ dinov2/fsdp/__init__.py
@@ -105,8 +105,8 @@ class FSDPCheckpointer(Checkpointer):
         basename = f"{name}.{rankstr()}.pth"
         save_file = os.path.join(self.save_dir, basename)
         assert os.path.basename(save_file) == basename, basename
+        self.logger.info("CODY {}".format(str(distributed.get_global_rank())))
         self.logger.info("Saving checkpoint to {}".format(save_file))
-        self.logger.info("Dist RANK {}".format(str(distributed.get_global_rank())))
         with self.path_manager.open(save_file, "wb") as f:
             torch.save(data, f)
         self.tag_last_checkpoint(basename)
